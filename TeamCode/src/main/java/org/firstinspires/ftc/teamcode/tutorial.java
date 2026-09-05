1428c79670441fbfacc75987caa91548e7b1f179
jacysylv1r <jacynguyen124@gmail.com>
2026-09-05 11:23:34 -0700

    second commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tutorial.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tutorial.java
@@ -4,7 +4,6 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp(name = "tutorial")
 public class tutorial extends OpMode {
-    @Override
     int integer_variable;
     public void init() {
         integer_variable = 1;
@@ -12,28 +11,22 @@ public class tutorial extends OpMode {
 
     @Override
     public void loop() {
-        if (integer_variable == 1)
-            integer_variable = 2
-
-        }else if (integer_variable == 2) {
-        integer_variable = 1
+        if (integer_variable == 1) {
+            integer_variable = 2;
+        } else if (integer_variable == 2) {
+            integer_variable = 1;
         }
-        if(gamepad.y)
-
-        {
-        //activate this code
-        }
-        if(gamepad.left_trigger >0.5)
-
-        {
+        if (gamepad1.y) {
             //activate this code
         }
-        if(gamepad.right_stick_x >1)
-
-        {
+        if (gamepad1.left_trigger > 0.5) {
             //activate this code
         }
-        if(gamepad2.a) {
+        if (gamepad1.right_stick_x > 1) {
             //activate this code
+        }
+        if (gamepad2.a) {
+            //activate this code
+        }
     }
 }
