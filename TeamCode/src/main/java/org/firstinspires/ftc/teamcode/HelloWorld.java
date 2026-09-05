b3fd4a6921dc664e4ca8b944219646b5116379e5
jacysylv1r <jacynguyen124@gmail.com>
2026-09-05 11:18:20 -0700

    second commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -0,0 +1,6 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+import com.qualcomm.robotcore.eventloop.opmode.OpMode public class HelloWorld extends OpMode {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tutorial.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+@TeleOp(name = "tutorial")
+public class tutorial extends OpMode {
+    @Override
+    int integer_variable;
+    public void init() {
+        integer_variable = 1;
+    }
+
+    @Override
+    public void loop() {
+        if (integer_variable == 1)
+            integer_variable = 2
+
+        }else if (integer_variable == 2) {
+        integer_variable = 1
+        }
+        if(gamepad.y)
+
+        {
+        //activate this code
+        }
+        if(gamepad.left_trigger >0.5)
+
+        {
+            //activate this code
+        }
+        if(gamepad.right_stick_x >1)
+
+        {
+            //activate this code
+        }
+        if(gamepad2.a) {
+            //activate this code
+    }
+}
